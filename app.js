e65c9130c0f8f05eba87ade771b56f127f209b9c
Mattias Erming <mattias@mattiaserming.com>
2014-03-04 08:31:52 -0800

    Implemented a simple web server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello, world.");
+var argv = require("commander")
+	.option("-p, --port <n>", "port to use", parseInt)
+	.parse(process.argv);
+
+PORT = 80; // Default port.
+if (argv.port) {
+	PORT = argv.port;
+}
+
+// Run the server!
+var Server = 
+	new (require("./lib/server.js"))()
+		.listen(PORT);
