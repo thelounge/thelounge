9cdcfdefd26c09b93a405a197643ce3382e5f1e0
Mattias Erming <mattias@mattiaserming.com>
2014-03-04 09:22:06 -0800

    Added socket.io

---
+++ client/js/client.js
@@ -0,0 +1,38 @@
+/**
+ * The Client class
+ *
+ * @public
+ */
+
+function Client() {
+
+	/**
+	 * Self references.
+	 *
+	 * @private
+	 */
+	
+	var self = this;
+	
+	/**
+	 * The active socket.
+	 *
+	 * @private
+	 */
+	
+	var socket = io.connect("")
+		.on("init", function(data) { self.init(data); });
+
+	/**
+	 * Setup new socket connections.
+	 *
+	 * @param {String} data
+	 * @public
+	 */
+	
+	this.init = function(data) {
+		// Debug
+		console.log(data);
+	};
+
+};
