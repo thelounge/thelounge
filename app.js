f13a6aa308b700616232a0998757a0ce48993368
Mattias Erming <mattias@mattiaserming.com>
2014-03-12 00:14:26 +0100

    Removing old comment

--- app.js
+++ app.js
@@ -10,23 +10,3 @@ if (argv.port) {
 var server =
 	new (require("./lib/server.js"))()
 		.listen(PORT);
-
-// Temp
-/*
-
-var models = require("./lib/models.js");
-var network = new models.Network({host: "irc.network.org"});
-
-server.networks.add(network);
-network.get("channels").add(new models.Channel({
-	name: "#foo",
-	messages: [
-		new models.Message({user: "user", text: "Hi!"}),
-		new models.Message({user: "user", text: ".. Hello?"}),
-	],
-	users: [
-		new models.User({name: "user"}),
-		new models.User({name: "other_user"}),
-	]:
-}));
-*/
