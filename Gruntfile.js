9e528e5f00be938916e8b1fe09193a3d4943a75f
Mattias Erming <mattias@mattiaserming.com>
2014-07-09 03:38:38 +0200

    Added node-webkit grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,34 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		uglify: {
+			options: {
+				compress: false
+			},
 			js: {
 				files: {
-					"client/js/components.min.js": [
-						"client/components/*.js",
-						"client/components/**/*.js"
-					]
+					"client/js/components.min.js": "client/components/**/*.js"
 				}
+			}
+		},
+		nodewebkit: {
+			src: "./**/*",
+			options: {
+				build_dir: "./build",
+				mac: false
 			}
 		}
 	});
 	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-node-webkit-builder");
+	grunt.registerTask(
+		"build",
+		function() {
+			require("child_process").exec("rm -rf ./build");
+			grunt.task.run("nodewebkit");
+		}
+	);
 	grunt.registerTask(
 		"default",
-		["uglify"]
+		"uglify"
 	);
 };
