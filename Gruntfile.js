a406e527211292508a0f637db8ca38e32505ee58
Mattias Erming <mattias@mattiaserming.com>
2014-10-08 21:15:57 +0200

    Compile templates with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,23 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 	grunt.registerTask(
+		"handlebars",
+		function() {
+			grunt.util.spawn({
+				cmd: "node",
+				args: [
+					"node_modules/handlebars/bin/handlebars",
+					"client/views/",
+					"-e", "tpl",
+					"-f", "client/js/shout.templates.js"
+				]
+			}, function(err) {
+				if (err) console.log(err);
+			});
+		}
+	);
+	grunt.registerTask(
 		"default",
-		["uglify"]
+		["uglify", "handlebars"]
 	);
 };
