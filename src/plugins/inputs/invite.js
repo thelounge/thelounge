14d722d387b5a40b5c7cdd4d6a523259fcc5fd50
Jérémie Astori <jeremie@astori.fr>
2016-02-20 05:21:21 +0000

    Add a simplified way to invite a user in the current channel

--- src/plugins/inputs/invite.js
+++ src/plugins/inputs/invite.js
@@ -2,8 +2,12 @@ module.exports = function(network, chan, cmd, args) {
 	if (cmd !== "invite") {
 		return;
 	}
+
 	var irc = network.irc;
+
 	if (args.length === 2) {
-		irc.invite(args[0], args[1]);
+		irc.invite(args[0], args[1]); // Channel provided in the command
+	}	else if (args.length === 1 && chan.type === "channel") {
+		irc.invite(args[0], chan.name); // Current channel
 	}
 };
