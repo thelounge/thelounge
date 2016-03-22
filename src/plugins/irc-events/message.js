6b2ebd790d2f62039ca6dda9980292312508ba7e
Pavel Djundik <github@xpaw.me>
2016-03-22 13:08:10 +0200

    Always send notices for non existing targets into server window
    This matches current master behaviour

--- src/plugins/irc-events/message.js
+++ src/plugins/irc-events/message.js
@@ -33,18 +33,16 @@ module.exports = function(irc, network) {
 			chan = network.channels[0];
 		} else {
 			var target = data.target;
-			var targetedAtUser = false;
 
 			// If the message is targeted at us, use sender as target instead
 			if (target.toLowerCase() === irc.user.nick.toLowerCase()) {
-				targetedAtUser = true;
 				target = data.nick;
 			}
 
 			var chan = network.getChannel(target);
 			if (typeof chan === "undefined") {
 				// Send notices that are not targeted at us into the server window
-				if (data.type === Msg.Type.NOTICE && !targetedAtUser) {
+				if (data.type === Msg.Type.NOTICE) {
 					chan = network.channels[0];
 				} else {
 					chan = new Chan({
