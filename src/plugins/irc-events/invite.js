142aa85548780b894938624ff89bd2eaff6df01a
Pavel Djundik <github@xpaw.me>
2016-03-26 01:26:53 +0200

    Handle invites
    Requires irc-fw update

--- src/plugins/irc-events/invite.js
+++ src/plugins/irc-events/invite.js
@@ -3,8 +3,6 @@ var Msg = require("../../models/msg");
 module.exports = function(irc, network) {
 	var client = this;
 	irc.on("invite", function(data) {
-		var target = data.to;
-
 		var chan = network.getChannel(data.channel);
 		if (typeof chan === "undefined") {
 			chan = network.channels[0];
@@ -12,10 +10,11 @@ module.exports = function(irc, network) {
 
 		var msg = new Msg({
 			type: Msg.Type.INVITE,
-			from: data.from,
-			target: target,
-			text: data.channel,
-			invitedYou: target === irc.user.nick
+			time: data.time,
+			from: data.nick,
+			invited: data.invited,
+			channel: data.channel,
+			invitedYou: data.invited === irc.user.nick
 		});
 		chan.messages.push(msg);
 		client.emit("msg", {
