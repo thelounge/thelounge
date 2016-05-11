8860b8421dc287ef27ed24b992c3419fbc1212f4
Pavel Djundik <github@xpaw.me>
2016-05-11 16:35:53 +0300

    Generate better nick colors, add support for specifying nick color luminosity

--- client/js/libs/handlebars/stringcolor.js
+++ client/js/libs/handlebars/stringcolor.js
@@ -1,5 +1,28 @@
-Handlebars.registerHelper(
-	"stringcolor", function(str) {
-		return stringcolor(str);
+(function() {
+	var cache = {};
+	var luminosity = document.body.dataset.nickLuminosity === 'light' ? 'light' : 'dark';
+
+	Handlebars.registerHelper(
+		"stringcolor", function(str) {
+			if (!cache[str]) {
+				cache[str] = randomColor({
+					seed: hashCode(str),
+					luminosity: luminosity
+				});
+			}
+
+			return cache[str];
+		}
+	);
+	
+	function hashCode(str) {
+		var hash = 0, i = 0;
+
+		for (; i < str.length; i++) {
+			hash = ((hash << 5) - hash) + str.charCodeAt(i);
+			hash |= 0;
+		}
+		
+		return Math.abs(hash);
 	}
-);
+}());
