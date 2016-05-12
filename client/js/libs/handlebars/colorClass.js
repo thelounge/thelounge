b3103e8c3f0d8ed5376763cf184619b7906ca5b1
Jérémie Astori <jeremie@astori.fr>
2016-05-12 02:29:36 -0400

    Add light colors for dark themes Morning and Zenburn
    These colors have been generated by the randomColor package

--- client/js/libs/handlebars/colorClass.js
+++ client/js/libs/handlebars/colorClass.js
@@ -1,13 +1,13 @@
 "use strict";
 
 Handlebars.registerHelper(
-	// Generates a string from "color-1" to "color-64" based on an input string
+	// Generates a string from "color-1" to "color-32" based on an input string
 	"colorClass", function(str) {
 		var hash = 0;
 		for (var i = 0; i < str.length; i++) {
 			hash += str.charCodeAt(i);
 		}
 
-		return "color-" + (1 + hash % 64);
+		return "color-" + (1 + hash % 32);
 	}
 );
