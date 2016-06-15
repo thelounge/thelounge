thelounge/thelounge

commit da54263b8e14814378b193abaeef35f8ebd218fa

Adds clarity to bind option default

defaults/config.js
@@ -28,6 +28,7 @@ module.exports = {
 
 	//
 	// Set the local IP to bind to.
+	// To listen on all IPs, set to undefined.
 	//
 	// @type     string
 	// @default  undefined

commit 18f0f44d1c273053a6438a88a1b343e9becf416d

Fix description of `host` and `bind` config options
Commit da54263b8e14814378b193abaeef35f8ebd218fa introduced a totally wrong description for those options. This one describes better what exactly those options do.

defaults/config.js
@@ -11,7 +11,8 @@ module.exports = {
 	public: true,
 
 	//
-	// Allow connections from this host.
+	// IP address or hostname for the web server to listen on.
+	// Setting this to "0.0.0.0" will listen on all interfaces.
 	//
 	// @type     string
 	// @default  "0.0.0.0"
@@ -27,8 +28,8 @@ module.exports = {
 	port: 9000,
 
 	//
-	// Set the local IP to bind to.
-	// To listen on all IPs, set to undefined.
+	// Set the local IP to bind to for outgoing connections. Leave to undefined
+	// to let the operating system pick its preferred one.
 	//
 	// @type     string
 	// @default  undefined