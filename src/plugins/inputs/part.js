f57328ef5ba30aaa413421c4d6f798bcb49b6ab0
Pavel Djundik <github@xpaw.me>
2016-07-03 11:39:29 +0300

    Only auto join actual channels
    Fixes #450

--- src/plugins/inputs/part.js
+++ src/plugins/inputs/part.js
@@ -1,11 +1,12 @@
 var _ = require("lodash");
 var Msg = require("../../models/msg");
+var Chan = require("../../models/chan");
 
 exports.commands = ["close", "leave", "part"];
 exports.allowDisconnected = true;
 
 exports.input = function(network, chan, cmd, args) {
-	if (chan.type === "lobby") {
+	if (chan.type === Chan.Type.LOBBY) {
 		chan.pushMessage(this, new Msg({
 			type: Msg.Type.ERROR,
 			text: "You can not part from networks, use /quit instead."
@@ -18,7 +19,7 @@ exports.input = function(network, chan, cmd, args) {
 		chan: chan.id
 	});
 
-	if (chan.type === "channel") {
+	if (chan.type === Chan.Type.CHANNEL) {
 		this.save();
 
 		if (network.irc) {
