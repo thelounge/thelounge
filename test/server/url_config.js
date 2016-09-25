9db7b1a50c40e2d74125b65710a48be528b70741
Doug Luce <doug@github.con.com>
2016-09-25 11:28:37 -0700

    Downconvert ES6 test code
    By request.

--- test/server/url_config.js
+++ test/server/url_config.js
@@ -5,11 +5,11 @@ var server = require("../../src/server");
 var Helper = require("../../src/helper");
 
 function OverrideDefaults() {
-	this.old_config = new Map();
+	this.old_config = {};
 }
 
-OverrideDefaults.prototype.set =	function(property, value) {
-	this.old_config.set(property, Helper.config.defaults[property]);
+OverrideDefaults.prototype.set = function(property, value) {
+	this.old_config[property] = Helper.config.defaults[property];
 	Helper.config.defaults[property] = value;
 };
 
@@ -19,16 +19,16 @@ OverrideDefaults.prototype.restore = function() {
 			Helper.config.defaults[property] = this.old_config[property];
 		}
 	}
-	this.old_config.clear();
+	this.old_config = {};
 };
 
 describe("URL-configurable parameters", function() {
 	before(function() {
 		Helper.config.port = 0;
 		Helper.config.enableOverrides = true;
-		global.log = {info: () => {}};
+		global.log = {info: function() {}};
 		this.server = server();
-		this.base_url = `http://localhost:${this.server.address().port}/`;
+		this.base_url = "http://localhost:" + this.server.address().port + "/";
 		this.override = new OverrideDefaults();
 	});
 
@@ -41,12 +41,13 @@ describe("URL-configurable parameters", function() {
 	});
 
 	it("should allow simple overriding", function(done) {
+		var that = this;
 		Helper.config.defaults.username = "DefaultUserName";
-		request.get(`${this.base_url}`, (err, res, body) => {
+		request.get(this.base_url, function (err, res, body) {
 			assert.equal(err, null);
 			assert.equal(res.statusCode, 200);
 			assert.equal(cheerio("input[name=username]", body).attr("value"), "DefaultUserName");
-			request.get(`${this.base_url}?username=URLUserName`, (err, res, body) => {
+			request.get(that.base_url + "?username=URLUserName", function (err, res, body) {
 				assert.equal(err, null);
 				assert.equal(res.statusCode, 200);
 				assert.equal(cheerio("input[name=username]", body).attr("value"), "URLUserName");
@@ -55,16 +56,17 @@ describe("URL-configurable parameters", function() {
 		});
 	});
 	it("should honor enableOverrides", function(done) {
+		var that = this;
 		this.override.set("host", "DefaultHost");
 		this.override.set("port", 443);
-		const config_url = `${this.base_url}?host=URLHost&port=12345`;
-		request.get(config_url, (err, res, body) => {
+		const config_url = this.base_url + "?host=URLHost&port=12345";
+		request.get(config_url, function (err, res, body) {
 			assert.equal(err, null);
 			assert.equal(res.statusCode, 200);
 			assert.equal(cheerio("input[name=host]", body).attr("value"), "URLHost");
 			assert.equal(cheerio("input[name=port]", body).attr("value"), 12345);
 			Helper.config.enableOverrides = false;
-			request.get(`${this.base_url}?host=URLHost`, (err, res, body) => {
+			request.get(that.base_url + "?host=URLHost", function (err, res, body) {
 				assert.equal(err, null);
 				assert.equal(res.statusCode, 200);
 				assert.equal(cheerio("input[name=host]", body).attr("value"), "DefaultHost");
@@ -75,16 +77,17 @@ describe("URL-configurable parameters", function() {
 		});
 	});
 	it("should honor lockNetwork", function(done) {
+		var that = this;
 		this.override.set("host", "DefaultHost");
 		this.override.set("port", 443);
-		const config_url = `${this.base_url}?host=URLHost&port=12345`;
-		request.get(config_url, (err, res, body) => {
+		const config_url = this.base_url + "?host=URLHost&port=12345";
+		request.get(config_url, function (err, res, body) {
 			assert.equal(err, null);
 			assert.equal(res.statusCode, 200);
 			assert.equal(cheerio("input[name=host]", body).attr("value"), "URLHost");
 			assert.equal(cheerio("input[name=port]", body).attr("value"), 12345);
 			Helper.config.lockNetwork = true;
-			request.get(`${this.base_url}?host=URLHost`, (err, res, body) => {
+			request.get(that.base_url + "?host=URLHost", function (err, res, body) {
 				assert.equal(err, null);
 				assert.equal(res.statusCode, 200);
 				assert.equal(cheerio("input[name=host]", body).attr("value"), "DefaultHost");
@@ -96,8 +99,8 @@ describe("URL-configurable parameters", function() {
 	});
 	it("should not allow stuffing inputs", function(done) {
 		this.override.set("nick", "DefaultNick");
-		const config_url = `${this.base_url}?nick="><script></script>`;
-		request.get(config_url, (err, res, body) => {
+		const config_url = this.base_url + '?nick="><script></script>';
+		request.get(config_url, function (err, res, body) {
 			assert.equal(err, null);
 			assert.equal(res.statusCode, 200);
 			assert.equal(cheerio("input[name=nick]", body).attr("value"), '"><script></script>');
