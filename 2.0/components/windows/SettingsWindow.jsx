cfa623ef9f9a28a5418d4cde350d4642735706b9
yofreke <sparkyjoe94@aim.com>
2016-09-27 19:55:21 -0700

    move missing JSX in to settings page

--- 2.0/components/windows/SettingsWindow.jsx
+++ 2.0/components/windows/SettingsWindow.jsx
@@ -1,7 +1,147 @@
 import React from 'react';
+import _ from 'lodash';
+
+// import { useState } from 'clientUI/utils/forms';
+
 
 export default class SettingsWindow extends React.Component {
+	constructor () {
+		super();
+	}
+
+	renderPrefetch () {
+		const prefetch = false; // TODO
+		if (!prefetch) {
+			return null;
+		}
+
+		return (
+			<div>
+				<div className="col-sm-12">
+					<h2>Links and URLs</h2>
+				</div>
+				<div className="col-sm-6">
+					<label className="opt">
+						<input type="checkbox" name="thumbnails" />
+						Auto-expand thumbnails
+					</label>
+				</div>
+				<div className="col-sm-6">
+					<label className="opt">
+						<input type="checkbox" name="links" />
+						Auto-expand links
+					</label>
+				</div>
+			</div>
+		);
+	}
+
+	renderPassword () {
+		const isPublic = false; // TODO
+		const ldap = { enable: false }; // TODO
+		if (isPublic && ldap.enable) {
+			return null;
+		}
+
+		return (
+			<div id="change-password">
+				<form>
+					<div className="col-sm-12">
+						<h2>Change password</h2>
+					</div>
+					<div className="col-sm-12">
+						<label
+							htmlFor="old_password_input"
+							className="sr-only"
+						>
+							Enter current password
+						</label>
+						<input
+							type="password"
+							id="old_password_input"
+							name="old_password"
+							className="input"
+							placeholder="Enter current password"
+						/>
+					</div>
+					<div className="col-sm-12">
+						<label
+							htmlFor="new_password_input"
+							className="sr-only"
+						>
+							Enter desired new password
+						</label>
+						<input
+							type="password"
+							id="new_password_input"
+							name="new_password"
+							className="input"
+							placeholder="Enter desired new password"
+						/>
+					</div>
+					<div className="col-sm-12">
+						<label
+							htmlFor="verify_password_input"
+							className="sr-only"
+						>
+							Repeat new password
+						</label>
+						<input
+							type="password"
+							id="verify_password_input"
+							name="verify_password"
+							className="input"
+							placeholder="Repeat new password"
+						/>
+					</div>
+					<div className="col-sm-12 feedback"></div>
+					<div className="col-sm-12">
+						<button type="submit" className="btn">Change password</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+
+	renderVersionInfo () {
+		const gitCommit = false; // TODO
+		const version = 'IDK THE VERSION LOL';
+
+		if (gitCommit) {
+			return (<p>
+				The Lounge is running from source
+				(
+				<a
+					href={'https://github.com/thelounge/lounge/tree/' + gitCommit}
+					target="_blank"
+				>
+					<code>{gitCommit}</code>
+				</a>
+				).
+				<br />
+			</p>);
+		}
+		return (<p>
+			The Lounge is in version <strong>{version}</strong>
+			(
+			<a
+				href={'https://github.com/thelounge/lounge/releases/tag/v' + version}
+				target="_blank"
+			>
+				See release notes
+			</a>
+			).
+			<br />
+		</p>);
+	}
+
 	render () {
+		const themes = [].map(themeName => ( // TODO
+			<option value={themeName}>
+				{_.capitalize(themeName)}
+			</option>
+		));
+
 		return (
 			<div id="settings" className="window">
 				<div className="header">
@@ -64,13 +204,19 @@ export default class SettingsWindow extends React.Component {
 							<h2>Theme</h2>
 						</div>
 						<div className="col-sm-12">
-							{/* attr: for="theme-select" */}
-							<label className="sr-only">Theme</label>
+							<label
+								className="sr-only"
+								htmlFor="theme-select"
+							>
+								Theme
+							</label>
 							<select id="theme-select" name="theme" className="input">
-								{/* TODO: PUT THE MISSING SNIPPET BACK */}
+								{themes}
 							</select>
 						</div>
-						{/* TODO: PUT THE MISSING SNIPPET BACK */}
+
+						{this.renderPrefetch()}
+
 						<div className="col-sm-12">
 							<h2>Notifications</h2>
 						</div>
@@ -102,13 +248,24 @@ export default class SettingsWindow extends React.Component {
 
 						<div className="col-sm-12">
 							<label className="opt">
-								{/* attr: for="highlights" */}
-								<label className="sr-only">Custom highlights (comma-separated keywords)</label>
-								<input type="text" id="highlights" name="highlights" className="input" placeholder="Custom highlights (comma-separated keywords)" />
+								<label
+									className="sr-only"
+									htmlFor="highlights"
+								>
+									Custom highlights (comma-separated keywords)
+								</label>
+								<input
+									type="text"
+									id="highlights"
+									name="highlights"
+									className="input"
+									placeholder="Custom highlights (comma-separated keywords)"
+								/>
 							</label>
 						</div>
 
-						{/* TODO: PUT THE MISSING SNIPPET BACK */}
+						{this.renderPassword()}
+
 						<div className="col-sm-12">
 							<h2>Custom Stylesheet</h2>
 						</div>
@@ -119,13 +276,13 @@ export default class SettingsWindow extends React.Component {
 							<h2>About The Lounge</h2>
 						</div>
 						<div className="col-sm-12">
-							<p className="about">
-								{/* TODO: PUT THE MISSING SNIPPET BACK */}
+							<div className="about">
+								{this.renderVersionInfo()}
 
 								<a href="https://thelounge.github.io/" target="_blank">Website</a><br />
 								<a href="https://thelounge.github.io/docs/" target="_blank">Documentation</a><br />
 								<a href="https://github.com/thelounge/lounge/issues/new" target="_blank">Report a bug</a>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
