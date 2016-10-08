e986a10331222dfd56134d5717cdc0f238eac5c6
Pavel Djundik <github@xpaw.me>
2016-10-08 15:22:00 +0300

    Prototype of server-side message search

--- src/server.js
+++ src/server.js
@@ -269,6 +269,12 @@ function init(socket, client) {
 			}
 		);
 		socket.on(
+			"search",
+			function(data) {
+				client.searchMessages(data);
+			}
+		);
+		socket.on(
 			"sort",
 			function(data) {
 				client.sort(data);
