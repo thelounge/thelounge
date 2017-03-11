23599fc39bbbc93be4c13de160535633a78cfd42
Pavel Djundik <github@xpaw.me>
2017-03-11 20:09:37 +0200

    Prevent message sending in lobbies
    Fixes #956

--- src/plugins/inputs/action.js
+++ src/plugins/inputs/action.js
@@ -1,8 +1,20 @@
 "use strict";
 
+var Chan = require("../../models/chan");
+var Msg = require("../../models/msg");
+
 exports.commands = ["slap", "me"];
 
 exports.input = function(network, chan, cmd, args) {
+	if (chan.type !== Chan.Type.CHANNEL && chan.type !== Chan.Type.QUERY) {
+		chan.pushMessage(this, new Msg({
+			type: Msg.Type.ERROR,
+			text: `${cmd} command can only be used in channels and queries.`
+		}));
+
+		return;
+	}
+
 	var irc = network.irc;
 	var text;
 
--- src/plugins/inputs/kick.js
+++ src/plugins/inputs/kick.js
@@ -1,8 +1,20 @@
 "use strict";
 
+var Chan = require("../../models/chan");
+var Msg = require("../../models/msg");
+
 exports.commands = ["kick"];
 
 exports.input = function(network, chan, cmd, args) {
+	if (chan.type !== Chan.Type.CHANNEL) {
+		chan.pushMessage(this, new Msg({
+			type: Msg.Type.ERROR,
+			text: `${cmd} command can only be used in channels.`
+		}));
+
+		return;
+	}
+
 	if (args.length !== 0) {
 		var irc = network.irc;
 		irc.raw("KICK", chan.name, args[0], args.slice(1).join(" "));
