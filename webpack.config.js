9bf1e6e0d52e9e4ffa52c5d0b8e32a9209e60154
Pavel Djundik <github@xpaw.me>
2017-03-19 10:02:39 +0200

    Do not build json3 module of Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,9 @@ let config = {
 			},
 		]
 	},
+	externals: {
+		json3: "JSON", // socket.io uses json3.js, but we do not target any browsers that need it
+	},
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin("js/bundle.vendor.js")
 	]
