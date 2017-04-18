5b4c00d8ca595a1c392f70893d0ca9830ce4028c
Jérémie Astori <jeremie@astori.fr>
2017-04-17 23:28:35 -0400

    Extract findLinks into its own file and add tests
    Tests were taken from https://github.com/Bonuspunkt/ircmessageparser/blob/5a249c30b1e0379e5df62f10734a3dc7ce02306b/test/findLinks.js.
    The underlying code is different but the tests are the same.

--- client/js/libs/handlebars/parse.js
+++ client/js/libs/handlebars/parse.js
@@ -1,58 +1,10 @@
 "use strict";
 
 const Handlebars = require("handlebars/runtime");
-const URI = require("urijs");
 const parseStyle = require("./ircmessageparser/parseStyle");
 const findChannels = require("./ircmessageparser/findChannels");
+const findLinks = require("./ircmessageparser/findLinks");
 const merge = require("./ircmessageparser/merge");
-
-const commonSchemes = [
-	"http", "https",
-	"ftp", "sftp",
-	"smb", "file",
-	"irc", "ircs",
-	"svn", "git",
-	"steam", "mumble", "ts3server",
-	"svn+ssh", "ssh",
-];
-
-function findLinks(text) {
-	let result = [];
-	let lastPosition = 0;
-
-	URI.withinString(text, function(url, start, end) {
-		// v-- fix: url was modified and does not match input string -> cant be mapped
-		if (text.indexOf(url, lastPosition) < 0) {
-			return;
-		}
-		// ^-- /fix: url was modified and does not match input string -> cant be mapped
-
-		// v-- fix: use prefered scheme
-		const parsed = URI(url);
-		const parsedScheme = parsed.scheme().toLowerCase();
-		const matchedScheme = commonSchemes.find(scheme => parsedScheme.endsWith(scheme));
-
-		if (matchedScheme) {
-			const prefix = parsedScheme.length - matchedScheme.length;
-			start += prefix;
-			url = url.slice(prefix);
-		}
-		// ^-- /fix: use prefered scheme
-
-		// URL matched, but does not start with a protocol, add it
-		if (!parsedScheme.length) {
-			url = "http://" + url;
-		}
-
-		result.push({
-			start: start,
-			end: end,
-			link: url
-		});
-	});
-
-	return result;
-}
 
 function createFragment(fragment) {
 	let className = "";
