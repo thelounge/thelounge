6eb6db1d0d76a220bd61513f253a6c7c18f358ee
Pavel Djundik <xPaw@users.noreply.github.com>
2017-04-28 20:53:22 +0300

    Add a default quit message

--- src/plugins/inputs/disconnect.js
+++ src/plugins/inputs/disconnect.js
@@ -1,9 +1,9 @@
 "use strict";
 
-exports.commands = ["disconnect"];
+exports.commands = ["disconnect", "quit"];
 
 exports.input = function(network, chan, cmd, args) {
-	var quitMessage = args[0] ? args.join(" ") : "";
+	var quitMessage = args[0] ? args.join(" ") : "The Lounge — https://thelounge.github.io/";
 
 	network.irc.quit(quitMessage);
 };
