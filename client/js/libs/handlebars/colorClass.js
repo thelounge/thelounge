e966eef1d50c0e74f7d275628a19c1704fd3ba8d
Niko Bews <deltebews@gmail.com>
2017-04-30 05:00:28 +0300

    Better user colors: 70 -> 64

--- client/js/libs/handlebars/colorClass.js
+++ client/js/libs/handlebars/colorClass.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// Generates a string from "color-1" to "color-32" based on an input string
+// Generates a string from "color-1" to "color-64" based on an input string
 module.exports = function(str) {
 	var hash = 0;
 	for (var i = 0; i < str.length; i++) {
 		hash += str.charCodeAt(i);
 	}
 
-	return "color-" + (1 + hash % 32);
+	return "color-" + (1 + hash % 64);
 };
