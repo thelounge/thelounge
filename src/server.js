torriz/lounge

Package system foundation

src/server.js
@@ -8,18 +8,40 @@ var fs = require("fs");
 var io = require("socket.io");
 var dns = require("dns");
 var Helper = require("./helper");
+var ServeStatic = require("serve-static");
 var config = {};
 
 var manager = new ClientManager();
+var packages = null;
+var stylesheets = [];
+var scripts = [];
 
 module.exports = function(options) {
 	config = Helper.getConfig();
 	config = _.extend(config, options);
+	packages = require("./packages");
 
 	var app = express()
 		.use(allRequests)
 		.use(index)
+		.use(clientPackages)
 		.use(express.static("client"));
+
+	packages.forEachProp("client", function(client, package) {
+		if ("stylesheets" in client && client.stylesheets instanceof Array) {
+			client.stylesheets.forEach(function(css) {
+				stylesheets.push(package.webroot + css);
+			});
+		}
+
+		if ("scripts" in client && client.scripts instanceof Array) {
+			client.scripts.forEach(function(script) {
+				scripts.push(package.webroot + script);
+			});
+		}
+	});
+
+	packages.emit("httpServer", app);
 
 	var server = null;
 	var https = config.https || {};
@@ -94,7 +116,11 @@ function index(req, res, next) {
 	return fs.readFile("client/index.html", "utf-8", function(err, file) {
 		var data = _.merge(
 			package,
-			config
+			config,
+			{
+				stylesheets: stylesheets,
+				scripts: scripts
+			}
 		);
 		var template = _.template(file);
 		res.setHeader("Content-Security-Policy", "default-src *; style-src * 'unsafe-inline'; script-src 'self'; child-src 'none'; object-src 'none'; form-action 'none'; referrer no-referrer;");
@@ -259,4 +285,13 @@ function auth(data) {
 			socket.emit("auth");
 		}
 	}
+}
+
+function clientPackages(req, res, next) {
+	if (!req.url.startsWith("/packages/")) {
+		return next();
+	}
+
+	req.url = req.url.replace(/^\/packages\/([^?&#]*?)\/(.*)$/, "/$1/client/$2");
+	ServeStatic("packages/")(req, res, next);
 }

thelounge/thelounge

src/server.js
@@ -14,9 +14,13 @@ var Helper = require("./helper");
 var ldap = require("ldapjs");
 var colors = require("colors/safe");
 const Identification = require("./identification");
+const ServeStatic = require("serve-static");
 
 var manager = null;
 var authFunction = localAuth;
+var packages = null;
+var stylesheets = [];
+var scripts = [];
 
 module.exports = function() {
 	log.info(`The Lounge ${colors.green(Helper.getVersion())} \
@@ -31,12 +35,31 @@ module.exports = function() {
 	var app = express()
 		.use(allRequests)
 		.use(index)
+		.use(clientPackages)
 		.use(express.static("client"))
 		.engine("html", expressHandlebars({extname: ".html"}))
 		.set("view engine", "html")
 		.set("views", path.join(__dirname, "..", "client"));
 
 	var config = Helper.config;
+
+	packages = require("./packages");
+	packages.forEachProp("client", function(pkgClient, pkgConfig) {
+		if ("stylesheets" in pkgClient && pkgClient.stylesheets instanceof Array) {
+			pkgClient.stylesheets.forEach(function(css) {
+				stylesheets.push(pkgConfig.webroot + css);
+			});
+		}
+
+		if ("scripts" in pkgClient && pkgClient.scripts instanceof Array) {
+			pkgClient.scripts.forEach(function(script) {
+				scripts.push(pkgConfig.webroot + script);
+			});
+		}
+	});
+
+	packages.emit("httpServer", app);
+
 	var server = null;
 
 	if (config.public && (config.ldap || {}).enable) {
@@ -131,6 +154,10 @@ function index(req, res, next) {
 	}
 
 	var data = _.merge(
+		{
+			stylesheets: stylesheets,
+			scripts: scripts
+		},
 		pkg,
 		Helper.config
 	);
@@ -373,4 +400,13 @@ function auth(data) {
 			authFunction(client, data.user, data.password, authCallback);
 		}
 	}
+}
+
+function clientPackages(req, res, next) {
+	if (!req.url.startsWith("/packages/")) {
+		return next();
+	}
+
+	req.url = req.url.replace(/^\/packages\/([^?&#]*?)\/(.*)$/, "/$1/client/$2");
+	ServeStatic("packages/")(req, res, next);
 }