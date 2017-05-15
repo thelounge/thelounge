b71a4ef5e1d079e575406b8058083034efd1a184
Niko Bews <deltebews@gmail.com>
2017-05-15 19:15:42 +0300

    Better user colors: regex fix

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -340,7 +340,7 @@ $(function() {
 			if (nicks) {
 				nicks.forEach(function(nick) {
 					if (data.msg.text.indexOf(nick) > -1) {
-						var re = new RegExp("(^| |&lt;)" + nick.replace(/\[/, "\\[").replace(/]/, "\\]") + "(.| |,|:|&gt;|$)", "g");
+						var re = new RegExp("(^| |&lt;)" + nick.replace(/\[/, "\\[").replace(/]/, "\\]") + "(\\.| |,|:|&gt;|$)", "g");
 						text.html(text.html().replace(re, "$1" + templates.user_name({nick: nick}).trim() + "$2"));
 					}
 				});
