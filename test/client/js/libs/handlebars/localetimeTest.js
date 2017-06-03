bd9e41f2c1bc8e5568ab642f8c94019b73e98c43
Pavel Djundik <github@xpaw.me>
2017-06-03 10:29:52 +0300

    Fix localtime test to correctly use UTC

--- test/client/js/libs/handlebars/localetimeTest.js
+++ test/client/js/libs/handlebars/localetimeTest.js
@@ -6,7 +6,7 @@ const localetime = require("../../../../../client/js/libs/handlebars/localetime"
 describe("localetime Handlebars helper", () => {
 	it("should render a human-readable date", () => {
 		// 12PM in UTC time
-		const date = new Date("2014-05-22T12:00:00");
+		const date = new Date("2014-05-22T12:00:00Z");
 
 		// Offset between UTC and local timezone
 		const offset = date.getTimezoneOffset() * 60 * 1000;
