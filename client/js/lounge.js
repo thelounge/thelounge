74ef68731cf208918eb08414c884df9bd9e3dbf5
Niko Bews <deltebews@gmail.com>
2017-06-10 21:11:37 +0300

    Enable socket reconnection: fix "new messages" marker

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -512,7 +512,16 @@ $(function() {
 		);
 		channels.forEach(renderChannel);
 
-		if (!loaded) {
+		if (loaded) {
+			let activeChan = chat.find(".chan.active");
+			socket.emit(
+				"open",
+				activeChan.data("id")
+			);
+			activeChan.find(".badge")
+				.removeClass("highlight")
+				.empty();
+		} else {
 			confirmExit();
 		}
 
