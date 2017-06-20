a403dac8ef6986e26868380548e4b989b97426f7
Jérémie Astori <jeremie@astori.fr>
2017-06-20 01:17:11 -0400

    Build in production environment on script testing PRs

--- scripts/run-pr.sh
+++ scripts/run-pr.sh
@@ -10,6 +10,6 @@ fi
 git fetch https://github.com/thelounge/lounge.git refs/pull/${1}/head
 git checkout FETCH_HEAD
 npm install
-npm run build
+NODE_ENV=production npm run build
 npm test || true
 npm start
