51c7aa89360735f2cf95d3beb03a4fe1d4a63cd8
Jan Visser <j.visser@scanmar.nl>
2017-06-22 09:38:42 +0200

    Bring back PR into a buildable state

--- client/js/clipboard.js
+++ client/js/clipboard.js
@@ -13,20 +13,20 @@ function copyMessages(event) {
 		copyText = selection.toString();
 	} else {
 		// Get the first range
-		let range = selection.getRangeAt(0);
-		let documentFragment = range.cloneContents();
-		let messages = documentFragment.querySelectorAll(".msg");
+		const range = selection.getRangeAt(0);
+		const documentFragment = range.cloneContents();
+		const messages = documentFragment.querySelectorAll(".msg");
 
 		messages.forEach(function(message) {
-			let el = $("#" + message.id);
+			const el = $("#" + message.id);
 			if (el) {
-				let time = el.find(".time").get(0).innerText;
+				const time = el.find(".time").get(0).innerText;
 				let from = "";
-				let fromEl = el.find(".from .user").get(0);
+				const fromEl = el.find(".from .user").get(0);
 				if (fromEl) {
 					from = fromEl.innerText;
 				}
-				let text = el.find(".text").get(0).innerText;
+				const text = el.find(".text").get(0).innerText;
 				if (from !== "") {
 					copyLines.push(time + " <" + from + "> " + text);
 				} else {
