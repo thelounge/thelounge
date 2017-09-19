matburnham/lounge

Group push notifications per target

client/service-worker.js
@@ -9,16 +9,30 @@ self.addEventListener("push", function(event) {
 
 	const payload = event.data.json();
 
-	if (payload.type === "notification") {
-		event.waitUntil(
-			self.registration.showNotification(payload.title, {
-				badge: "img/logo-64.png",
-				icon: "img/touch-icon-192x192.png",
-				body: payload.body,
-				timestamp: payload.timestamp,
-			})
-		);
+	if (payload.type !== "notification") {
+		return;
 	}
+
+	// get current notification, close it, and draw new
+	event.waitUntil(
+		self.registration
+			.getNotifications({
+				tag: `lounge-${payload.chanId}`
+			})
+			.then((notifications) => {
+				for (const notification of notifications) {
+					notification.close();
+				}
+
+				return self.registration.showNotification(payload.title, {
+					tag: `lounge-${payload.chanId}`,
+					badge: "img/logo-64.png",
+					icon: "img/touch-icon-192x192.png",
+					body: payload.body,
+					timestamp: payload.timestamp,
+				});
+			})
+	);
 });
 
 self.addEventListener("notificationclick", function(event) {

thelounge/thelounge

client/service-worker.js
@@ -9,16 +9,30 @@ self.addEventListener("push", function(event) {
 
 	const payload = event.data.json();
 
-	if (payload.type === "notification") {
-		event.waitUntil(
-			self.registration.showNotification(payload.title, {
-				badge: "img/logo-64.png",
-				icon: "img/touch-icon-192x192.png",
-				body: payload.body,
-				timestamp: payload.timestamp,
-			})
-		);
+	if (payload.type !== "notification") {
+		return;
 	}
+
+	// get current notification, close it, and draw new
+	event.waitUntil(
+		self.registration
+			.getNotifications({
+				tag: `chan-${payload.chanId}`
+			})
+			.then((notifications) => {
+				for (const notification of notifications) {
+					notification.close();
+				}
+
+				return self.registration.showNotification(payload.title, {
+					tag: `chan-${payload.chanId}`,
+					badge: "img/logo-64.png",
+					icon: "img/touch-icon-192x192.png",
+					body: payload.body,
+					timestamp: payload.timestamp,
+				});
+			})
+	);
 });
 
 self.addEventListener("notificationclick", function(event) {