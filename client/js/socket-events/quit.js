99c8e1c04a85a39d39c2898cde3e493128c89c32
PolarizedIons <PolarizedIons@gmail.com>
2017-10-02 13:38:34 +0200

    Remove channel containers from DOM after quitting network

--- client/js/socket-events/quit.js
+++ client/js/socket-events/quit.js
@@ -1,12 +1,20 @@
 "use strict";
 
 const $ = require("jquery");
+const chat = $("#chat");
 const socket = require("../socket");
 const sidebar = $("#sidebar");
 
 socket.on("quit", function(data) {
 	const id = data.network;
-	sidebar.find("#network-" + id).remove();
+	const network = sidebar.find("#network-" + id);
+
+	network.children(".chan").each(function() {
+		// this = child
+		chat.find($(this).data("target")).remove();
+	});
+
+	network.remove();
 
 	const chan = sidebar.find(".chan")
 		.eq(0)
