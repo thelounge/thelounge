e719e4ff81edd4d1bfb59981454f2a990fc4970a
Pavel Djundik <github@xpaw.me>
2018-03-04 22:03:11 +0200

    Cleanup chat/userlist to use flexbox, fix a couple of bugs

--- client/js/userlist.js
+++ client/js/userlist.js
@@ -8,9 +8,9 @@ const templates = require("../views");
 
 const chat = $("#chat");
 
-chat.on("input", ".users .search", function() {
+chat.on("input", ".userlist .search", function() {
 	const value = $(this).val();
-	const parent = $(this).closest(".users");
+	const parent = $(this).closest(".userlist");
 	const names = parent.find(".names-original");
 	const container = parent.find(".names-filtered");
 
@@ -42,22 +42,24 @@ chat.on("input", ".users .search", function() {
 	container.find(".user").first().addClass("active");
 });
 
-chat.on("mouseenter", ".users .user", function() {
+chat.on("mouseenter", ".userlist .user", function() {
 	// Reset any potential selection, this is required in cas there is already a
 	// nick previously selected by keyboard
-	$(".users .user").removeClass("active");
+	$(this).parent().find(".user.active").removeClass("active");
 
 	$(this).addClass("active");
 });
 
-chat.on("mouseleave", ".users .user", function() {
+chat.on("mouseleave", ".userlist .user", function() {
 	// Reset any potential selection
-	$(".users .user").removeClass("active");
+	$(this).parent().find(".user.active").removeClass("active");
 });
 
 exports.handleKeybinds = function(input) {
-	Mousetrap(input.get(0)).bind(["up", "down"], (_e, key) => {
-		const userlists = input.closest(".users");
+	Mousetrap(input.get(0)).bind(["up", "down"], (e, key) => {
+		e.preventDefault();
+
+		const userlists = input.closest(".userlist");
 		let userlist;
 
 		// If input field has content, use the filtered list instead
@@ -69,13 +71,17 @@ exports.handleKeybinds = function(input) {
 
 		const users = userlist.find(".user");
 
+		if (users.length === 0) {
+			return;
+		}
+
 		// Find which item in the array of users is currently selected, if any.
 		// Returns -1 if none.
 		const activeIndex = users.toArray()
 			.findIndex((user) => user.classList.contains("active"));
 
 		// Now that we know which user is active, reset any selection
-		userlists.find(".user").removeClass("active");
+		userlist.find(".user.active").removeClass("active");
 
 		// Mark next/previous user as active.
 		if (key === "down") {
@@ -87,23 +93,13 @@ exports.handleKeybinds = function(input) {
 		}
 
 		// Adjust scroll when active item is outside of the visible area
-		const userlistHeight = userlist.height();
-		const userlistScroll = userlist.scrollTop();
-		const active = $(".user.active");
-		const activeTop = active.position().top;
-		const activeHeight = active.height();
-
-		if (activeTop > userlistHeight - activeHeight) {
-			userlist.scrollTop(userlistScroll + activeTop - userlistHeight + activeHeight);
-		} else if (activeTop < 0) {
-			userlist.scrollTop(userlistScroll + activeTop - activeHeight);
-		}
+		userlist.find(".user.active")[0].scrollIntoView(false);
 	});
 
 	// When pressing Enter, open the context menu (emit a click) on the active
 	// user
 	Mousetrap(input.get(0)).bind("enter", () => {
-		const user = input.closest(".users").find(".user.active");
+		const user = input.closest(".userlist").find(".user.active");
 
 		if (user.length) {
 			const clickEvent = new $.Event("click");
