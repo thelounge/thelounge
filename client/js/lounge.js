1229371734a64c3068c71699272a10800b5a1809
Richard Lewis <richrd.lewis@gmail.com>
2018-07-06 22:52:23 +0300

    Fix chrome autocomplete bug.

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -297,10 +297,15 @@ $(function() {
 		}
 	});
 
+	// Handle search box
 	$("#windows").on("click", ".header button.search", (e) => {
 		e.preventDefault();
 		$(".window .active .header div.search .input-container").toggleClass("open");
-		$(".window .active .header div.search .input-container input").focus();
+		// Only focus the field after the CSS animation has completed.
+		// This prevents the chrome autocomplete appearing on top of the input on Android.
+		setTimeout(function() {
+			$(".window .active .header div.search .input-container input").focus();
+		}, 300);
 	});
 
 	// Compute how many milliseconds are remaining until the next day starts
