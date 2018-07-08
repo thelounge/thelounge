5ff8beb740e34254c756dc4bfdfa2536ff14c10c
Pavel Djundik <github@xpaw.me>
2018-07-08 20:35:46 +0300

    Delete some files and undo server change

--- client/js/libs/handlebars/slugify.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const escape = require("css.escape");
-
-module.exports = function(orig) {
-	return escape(orig.toLowerCase());
-};
--- client/js/libs/handlebars/tojson.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = function tojson(context) {
-	return JSON.stringify(context);
-};
