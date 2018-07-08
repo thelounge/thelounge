a229138ca65ff8eee0dbc3e4b4933a496b3a9a51
Pavel Djundik <github@xpaw.me>
2018-07-08 20:53:23 +0300

    Add connected state

--- client/js/vue.js
+++ client/js/vue.js
@@ -21,6 +21,7 @@ Vue.filter("roundBadgeNumber", roundBadgeNumber);
 const vueApp = new Vue({
 	el: "#viewport",
 	data: {
+		connected: false,
 		appName: document.title,
 		activeChannel: null,
 		networks: [],
