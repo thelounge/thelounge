e61a6681c34d0e88acce77a472b184d64ef154d0
Pavel Djundik <github@xpaw.me>
2018-07-08 22:02:36 +0300

    Implement user search in vue

---
+++ client/components/UsernameFiltered.vue
@@ -0,0 +1,15 @@
+<template>
+	<span
+		:class="['user', $options.filters.colorClass(user.original.nick)]"
+		:data-name="user.original.nick"
+		role="button" v-html="user.original.mode + user.string"/>
+</template>
+
+<script>
+export default {
+	name: "UsernameFiltered",
+	props: {
+		user: Object,
+	},
+};
+</script>
