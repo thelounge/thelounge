3b99d5a15613f453a0033c6201f770c717585be2
Pavel Djundik <github@xpaw.me>
2018-07-10 10:49:23 +0300

    Refactor client side commands
    Co-Authored-By: Max Leiter <hello@maxleiter.com>

---
+++ client/js/commands/join.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const $ = require("jquery");
+
+exports.input = function(args) {
+	console.log(args);
+	const channel = args[0];
+	const utils = require("../utils");
+	const socket = require("../socket");
+	const {vueApp} = require("../vue");
+
+	if (channel) {
+		const chan = utils.findCurrentNetworkChan(channel);
+
+		if (chan) {
+			$(`#sidebar .chan[data-id="${chan.id}"]`).trigger("click");
+		}
+	} else if (vueApp.activeChannel.channel.type === "channel") {
+		socket.emit("input", {
+			target: vueApp.activeChannel.channel.id,
+			text: `/join ${vueApp.activeChannel.channel.name}`,
+		});
+
+		return true;
+	}
+};
