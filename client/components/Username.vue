92fdf3cd5f2d9f244e80e2bbadd0fdf299bd4b52
Pavel Djundik <github@xpaw.me>
2018-07-11 10:34:11 +0300

    Use vue's conditional classes

--- client/components/Username.vue
+++ client/components/Username.vue
@@ -1,6 +1,6 @@
 <template>
 	<span
-		:class="['user', $options.filters.colorClass(user.nick), active ? 'active' : '']"
+		:class="['user', $options.filters.colorClass(user.nick), { active: active }]"
 		:data-name="user.nick"
 		role="button"
 		v-on="onHover ? { mouseover: hover } : {}">{{ user.mode }}{{ user.nick }}</span>
