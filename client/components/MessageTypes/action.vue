825e3beba6ba2fec47ccdbf2be89e9228c53c05d
Pavel Djundik <github@xpaw.me>
2018-07-12 19:59:13 +0300

    Show previews in actions

--- client/components/MessageTypes/action.vue
+++ client/components/MessageTypes/action.vue
@@ -4,17 +4,23 @@
 		<span
 			ref="text"
 			class="text"><ParsedMessage :message="message"/></span>
+		<LinkPreview
+			v-for="preview in message.previews"
+			:key="preview.link"
+			:link="preview"/>
 	</span>
 </template>
 
 <script>
 import ParsedMessage from "../ParsedMessage.vue";
+import LinkPreview from "../LinkPreview.vue";
 import Username from "../Username.vue";
 
 export default {
 	name: "MessageTypeAction",
 	components: {
 		ParsedMessage,
+		LinkPreview,
 		Username,
 	},
 	props: {
