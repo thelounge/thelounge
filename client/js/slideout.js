77cd443164d832fcba097dcb5f2764ad60ade553
Pavel Djundik <github@xpaw.me>
2018-07-13 10:41:12 +0300

    Fix menu swiping

--- client/js/slideout.js
+++ client/js/slideout.js
@@ -34,7 +34,7 @@ function onTouchStart(e) {
 	}
 
 	const touch = e.touches.item(0);
-	const styles = window.getComputedStyle(this.menu);
+	const styles = window.getComputedStyle(SlideoutMenu.menu);
 
 	menuWidth = parseFloat(styles.width);
 	menuIsAbsolute = styles.position === "absolute";
@@ -66,7 +66,7 @@ function onTouchMove(e) {
 		const devicePixelRatio = window.devicePixelRatio || 2;
 
 		if (Math.abs(distX) > devicePixelRatio) {
-			this.viewport.classList.toggle("menu-dragging", true);
+			SlideoutMenu.viewport.classList.toggle("menu-dragging", true);
 			menuIsMoving = true;
 		}
 	}
@@ -86,8 +86,8 @@ function onTouchMove(e) {
 		distX = 0;
 	}
 
-	this.menu.style.transform = "translate3d(" + distX + "px, 0, 0)";
-	this.sidebarOverlay.style.opacity = distX / menuWidth;
+	SlideoutMenu.menu.style.transform = "translate3d(" + distX + "px, 0, 0)";
+	SlideoutMenu.sidebarOverlay.style.opacity = distX / menuWidth;
 }
 
 function onTouchEnd() {
@@ -100,9 +100,9 @@ function onTouchEnd() {
 
 	document.body.removeEventListener("touchmove", onTouchMove);
 	document.body.removeEventListener("touchend", onTouchEnd);
-	this.viewport.classList.toggle("menu-dragging", false);
-	this.menu.style.transform = null;
-	this.sidebarOverlay.style.opacity = null;
+	SlideoutMenu.viewport.classList.toggle("menu-dragging", false);
+	SlideoutMenu.menu.style.transform = null;
+	SlideoutMenu.sidebarOverlay.style.opacity = null;
 
 	touchStartPos = null;
 	touchCurPos = null;
