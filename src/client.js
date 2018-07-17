8e5baf9af41ddf7a6f9988f1dc3254888b53f7a8
Pavel Djundik <github@xpaw.me>
2018-07-17 11:41:12 +0300

    Fix unread marker being off by one on the server

--- src/client.js
+++ src/client.js
@@ -410,9 +410,12 @@ Client.prototype.open = function(socketId, target) {
 		return;
 	}
 
-	target.chan.firstUnread = 0;
 	target.chan.unread = 0;
 	target.chan.highlight = 0;
+
+	if (target.chan.messages.length > 0) {
+		target.chan.firstUnread = target.chan.messages[target.chan.messages.length - 1].id;
+	}
 
 	this.attachedClients[socketId].openChannel = target.chan.id;
 	this.lastActiveChannel = target.chan.id;
