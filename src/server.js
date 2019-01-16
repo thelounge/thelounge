e80b058550a8dd893226b02f26414b129514e790
Pavel Djundik <xPaw@users.noreply.github.com>
2019-01-16 10:59:52 +0200

    Don't allow setting values to be objects

--- src/server.js
+++ src/server.js
@@ -494,7 +494,7 @@ function initializeClient(socket, client, token, lastMessage) {
 				return;
 			}
 
-			if (typeof newSetting.name !== "string" || newSetting.name[0] === "_") {
+			if (typeof newSetting.value === "object" || typeof newSetting.name !== "string" || newSetting.name[0] === "_") {
 				return;
 			}
 
