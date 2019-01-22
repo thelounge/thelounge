85764df7d0fc3bfe88315b3eb17b637e023127a1
Pavel Djundik <xPaw@users.noreply.github.com>
2019-01-22 16:33:20 +0200

    Fix hasRoleInChannel

--- client/js/utils.js
+++ client/js/utils.js
@@ -37,7 +37,7 @@ function hasRoleInChannel(channel, roles, nick) {
 	const channelID = channel.attr("data-id");
 	const network = $("#sidebar .network").has(`.chan[data-id="${channelID}"]`);
 	const target = nick || network.attr("data-nick");
-	const user = channel.find(`.names-original .user[data-name="${escape(target)}"]`).first();
+	const user = channel.find(`.names .user[data-name="${escape(target)}"]`).first();
 	return user.parent().is("." + roles.join(", ."));
 }
 
