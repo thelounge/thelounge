4765f0694061f183a912a8f5605243fe8efa0539
Pavel Djundik <xPaw@users.noreply.github.com>
2019-07-03 11:20:29 +0300

    Make sure unhandled command has params

--- src/plugins/irc-events/unhandled.js
+++ src/plugins/irc-events/unhandled.js
@@ -8,16 +8,18 @@ module.exports = function(irc, network) {
 	irc.on("unknown command", function(command) {
 		let target = network.channels[0];
 
-		// Do not display users own name
-		if (command.params[0] === network.irc.user.nick) {
-			command.params.shift();
-		} else {
-			// If this numeric starts with a channel name that exists
-			// put this message in that channel
-			const channel = network.getChannel(command.params[0]);
+		if (command.params.length > 0) {
+			// Do not display users own name
+			if (command.params[0] === network.irc.user.nick) {
+				command.params.shift();
+			} else {
+				// If this numeric starts with a channel name that exists
+				// put this message in that channel
+				const channel = network.getChannel(command.params[0]);
 
-			if (typeof channel !== "undefined") {
-				target = channel;
+				if (typeof channel !== "undefined") {
+					target = channel;
+				}
 			}
 		}
 
