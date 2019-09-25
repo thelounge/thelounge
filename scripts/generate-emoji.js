55427cd403dc04a777220c5d13639b4ac54d4244
Azhar Mithani <azharmithani07@gmail.com>
2019-09-25 14:37:14 -0700

    Update generate-emoji.js

--- scripts/generate-emoji.js
+++ scripts/generate-emoji.js
@@ -3,7 +3,7 @@
 const got = require("got");
 const path = require("path");
 const fs = require("fs");
-
+//importing the emogi.json 
 (async () => {
 	const response = await got(
 		"https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json"
@@ -11,7 +11,7 @@ const fs = require("fs");
 	const emojiStrategy = JSON.parse(response.body);
 	const emojiMap = {};
 	const fullNameEmojiMap = {};
-
+// description of emogi 
 	for (const emoji of emojiStrategy) {
 		fullNameEmojiMap[emoji.emoji] = emoji.description;
 
@@ -22,12 +22,12 @@ const fs = require("fs");
 
 	const emojiMapOutput = JSON.stringify(emojiMap, null, 2) + "\n";
 	const fullNameEmojiMapOutput = JSON.stringify(fullNameEmojiMap, null, 2) + "\n";
-
+//syncing the simplemap.json
 	fs.writeFileSync(
 		path.resolve(path.join(__dirname, "..", "client", "js", "libs", "simplemap.json")),
 		emojiMapOutput
 	);
-
+//syncing the fullnamemap.json
 	fs.writeFileSync(
 		path.resolve(path.join(__dirname, "..", "client", "js", "libs", "fullnamemap.json")),
 		fullNameEmojiMapOutput
