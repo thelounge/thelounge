d2f0590c730cf46ed4ba527678647a799e85a6f5
Richard Lewis <richrd.lewis@gmail.com>
2019-12-19 13:22:04 +0000

    Fix class targeting

--- client/components/ChannelWrapper.vue
+++ client/components/ChannelWrapper.vue
@@ -8,8 +8,11 @@
 			{active: activeChannel && channel === activeChannel.channel},
 			{'parted-channel': channel.type === 'channel' && channel.state === 0},
 			{'has-draft': channel.pendingMessage},
-			{'not-secure': network.status.connected && !network.status.secure},
-			{'not-connected': !network.status.connected},
+			{
+				'not-secure':
+					channel.type === 'lobby' && network.status.connected && !network.status.secure,
+			},
+			{'not-connected': channel.type === 'lobby' && !network.status.connected},
 		]"
 		:aria-label="getAriaLabel()"
 		:title="getAriaLabel()"
