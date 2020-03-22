4ce2efe86bbe6565d0ae494c85a52a3201568ca3
Pavel Djundik <xPaw@users.noreply.github.com>
2020-03-22 12:47:41 +0200

    Upgrade mocha and remove mochapack

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,11 +1,24 @@
 "use strict";
 
 const webpack = require("webpack");
+const fs = require("fs");
 const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const config = require("./webpack.config.js");
+
+const testFile = path.resolve(__dirname, "test/public/testclient.js");
+
+if (fs.existsSync(testFile)) {
+	fs.unlinkSync(testFile);
+}
 
 config.target = "node";
 config.devtool = "eval";
+config.stats = "errors-only";
+config.output.path = path.resolve(__dirname, "test/public");
+config.entry = {
+	"testclient.js": [path.resolve(__dirname, "test/client/index.js")],
+};
 
 // Add the istanbul plugin to babel-loader options
 for (const rule of config.module.rules) {
@@ -19,15 +32,18 @@ for (const rule of config.module.rules) {
 // - https://github.com/webpack/webpack/issues/6727#issuecomment-372589122
 config.optimization.splitChunks = false;
 
-// "Fixes" Critical dependency: the request of a dependency is an expression
-config.plugins.push(new webpack.ContextReplacementPlugin(/vue-server-renderer$/));
+// Disable plugins like copy files, it is not required
+config.plugins = [
+	new VueLoaderPlugin(),
 
-// Client tests that require Vue may end up requireing socket.io
-config.plugins.push(
+	// Client tests that require Vue may end up requireing socket.io
 	new webpack.NormalModuleReplacementPlugin(
 		/js(\/|\\)socket\.js/,
 		path.resolve(__dirname, "scripts/noop.js")
-	)
-);
+	),
+
+	// "Fixes" Critical dependency: the request of a dependency is an expression
+	new webpack.ContextReplacementPlugin(/vue-server-renderer$/),
+];
 
 module.exports = config;
