bd4e8216141832929edf41483e06792d9ce73d1a
Nachtalb <info@nachtalb.io>
2021-05-01 01:36:44 +0200

    Improve readability of more.js

--- client/js/socket-events/more.js
+++ client/js/socket-events/more.js
@@ -6,17 +6,17 @@ import socket from "../socket";
 import store from "../store";
 
 socket.on("more", function (data) {
-	const channel = store.getters.findChannel(data.chan);
+	const channel = store.getters.findChannel(data.chan).channel;
 
 	if (!channel) {
 		return;
 	}
 
-	channel.channel.moreHistoryAvailable =
-		data.totalMessages > channel.channel.messages.length + data.messages.length;
-	channel.channel.messages.unshift(...data.messages);
+	channel.moreHistoryAvailable =
+		data.totalMessages > channel.messages.length + data.messages.length;
+	channel.messages.unshift(...data.messages);
 
 	Vue.nextTick(() => {
-		channel.channel.historyLoading = false;
+		channel.historyLoading = false;
 	});
 });
