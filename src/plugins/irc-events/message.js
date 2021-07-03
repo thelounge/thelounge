16177eb9f4a630c83e0c4fc241b2293ec4966d46
JeDaYoshi <hi@jeda.im>
2021-07-03 21:06:16 +0000

    Move server nick code to handleMessage

--- src/plugins/irc-events/message.js
+++ src/plugins/irc-events/message.js
@@ -11,12 +11,6 @@ module.exports = function (irc, network) {
 	const client = this;
 
 	irc.on("notice", function (data) {
-		// Some servers send notices without any nickname
-		if (!data.nick) {
-			data.from_server = true;
-			data.nick = data.hostname || network.host;
-		}
-
 		data.type = Msg.Type.NOTICE;
 		handleMessage(data);
 	});
@@ -27,12 +21,6 @@ module.exports = function (irc, network) {
 	});
 
 	irc.on("privmsg", function (data) {
-		// Some servers send messages without any nickname
-		if (!data.nick) {
-			data.from_server = true;
-			data.nick = data.hostname || network.host;
-		}
-
 		data.type = Msg.Type.MESSAGE;
 		handleMessage(data);
 	});
@@ -49,6 +37,12 @@ module.exports = function (irc, network) {
 		let highlight = false;
 		let showInActive = false;
 		const self = data.nick === irc.user.nick;
+
+		// Some servers send messages without any nickname
+		if (!data.nick) {
+			data.from_server = true;
+			data.nick = data.hostname || network.host;
+		}
 
 		// Check if the sender is in our ignore list
 		const shouldIgnore =
