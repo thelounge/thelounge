beb5530c6518b759b473c6664a344cd729f78b30
Max Leiter <maxwell.leiter@gmail.com>
2021-08-31 12:27:43 -0700

    Revert "Support animated webp images" (#4287)
    This reverts pull/4186.

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,6 @@ const config = {
 	},
 	externals: {
 		json3: "JSON", // socket.io uses json3.js, but we do not target any browsers that need it
-		sharp: "commonjs sharp",
 	},
 	plugins: [
 		new VueLoaderPlugin(),
