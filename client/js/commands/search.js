7cd026a0d4b70a3891a3ab53129a7d491c490811
Max Leiter <maxwell.leiter@gmail.com>
2021-11-15 13:57:53 -0800

    Simplify checking if search enabled

--- client/js/commands/search.js
+++ client/js/commands/search.js
@@ -2,28 +2,22 @@
 
 import store from "../store";
 import {router} from "../router";
-const Msg = require("../../../src/models/msg");
 
 function input(args) {
-	const {channel} = store.state.activeChannel;
-
 	if (!store.state.settings.searchEnabled) {
-		const message = new Msg({
-			type: Msg.Type.ERROR,
-			text: "Search is currently not enabled.",
-		});
-		channel.messages.push(message);
-	} else {
-		router.push({
-			name: "SearchResults",
-			params: {
-				id: channel.id,
-			},
-			query: {
-				q: args.join(" "),
-			},
-		});
+		return true;
 	}
+
+	const {channel} = store.state.activeChannel;
+	router.push({
+		name: "SearchResults",
+		params: {
+			id: channel.id,
+		},
+		query: {
+			q: args.join(" "),
+		},
+	});
 
 	return true;
 }
