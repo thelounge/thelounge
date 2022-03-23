38a07e255ac046b7d8983271781bb09dd4a56620
kinghat <kinghat@protonmail.com>
2022-03-23 13:00:48 -0500

    change keepalive command and use container volume env

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# chown -R node:node "${THELOUNGE_HOME}"/.thelounge
-chown -R node:node /home/node/.thelounge
+chown -R node:node "${THELOUNGE_HOME}"
 
-while sleep 1000; do :; done
-
-# sleep infinity
+sleep infinity
