815319810c28ffe17119a5dc62f7eac33eba12f5
Reto <reto@labrat.space>
2022-04-12 02:46:29 +0200

    cli: don't error if the user folder doesn't exist (#4508)
    The user folder gets created on demand, thelounge list should not
    fail if the folder doesn't exist.
    This just means that no users are present, so report that instead.

--- src/command-line/users/list.js
+++ src/command-line/users/list.js
@@ -3,8 +3,6 @@
 const log = require("../../log");
 const colors = require("chalk");
 const program = require("commander");
-const fs = require("fs");
-const Helper = require("../../helper");
 const Utils = require("../utils");
 
 program
@@ -12,11 +10,6 @@ program
 	.description("List all users")
 	.on("--help", Utils.extraHelp)
 	.action(function () {
-		if (!fs.existsSync(Helper.getUsersPath())) {
-			log.error(`${Helper.getUsersPath()} does not exist.`);
-			return;
-		}
-
 		const ClientManager = require("../../clientManager");
 		const users = new ClientManager().getUsers();
 
@@ -25,16 +18,17 @@ program
 			return;
 		}
 
-		if (users.length > 0) {
-			log.info("Users:");
-			users.forEach((user, i) => {
-				log.info(`${i + 1}. ${colors.bold(user)}`);
-			});
-		} else {
+		if (users.length === 0) {
 			log.info(
 				`There are currently no users. Create one with ${colors.bold(
 					"thelounge add <name>"
 				)}.`
 			);
+			return;
 		}
+
+		log.info("Users:");
+		users.forEach((user, i) => {
+			log.info(`${i + 1}. ${colors.bold(user)}`);
+		});
 	});
