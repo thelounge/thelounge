c3e906c279c92a41baac0a35efcc1e152d091c37
Max Leiter <maxwell.leiter@gmail.com>
2022-05-02 00:53:35 -0700

    gitignore client/dist

--- client/dist/js/helpers/localetime.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-import dayjs from "dayjs";
-export default (time) => dayjs(time).format("D MMMM YYYY, HH:mm:ss");
-//# sourceMappingURL=localetime.js.map
--- client/dist/js/socket-events/sign_out.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-import socket from "../socket";
-import Auth from "../auth";
-socket.on("sign-out", function () {
-	Auth.signout();
-});
-//# sourceMappingURL=sign_out.js.map
