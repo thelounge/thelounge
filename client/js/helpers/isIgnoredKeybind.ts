89919edfafdabcd54720dba2677f52b08ef07efb
Max Leiter <maxwell.leiter@gmail.com>
2022-05-02 21:16:01 -0700

    [ts-migrate][client] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- client/js/helpers/isIgnoredKeybind.ts
+++ client/js/helpers/isIgnoredKeybind.ts
@@ -1,12 +1,15 @@
 "use strict";
 
-export default (event) => {
-	if (event.target.tagName !== "TEXTAREA" && event.target.tagName !== "INPUT") {
+export default (event: MouseEvent) => {
+	if (
+		(event.target as HTMLElement).tagName !== "TEXTAREA" &&
+		(event.target as HTMLElement).tagName !== "INPUT"
+	) {
 		return false;
 	}
 
 	// If focus is in a textarea, do not handle keybinds if user has typed anything
 	// This is done to prevent keyboard layout binds conflicting with ours
 	// For example alt+shift+left on macos selects a word
-	return !!event.target.value;
+	return !!(event.target as any).value;
 };
