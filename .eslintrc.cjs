b798cfdc64dacfa3ed37be21f2b8e43067d22d28
Eric Nemchik <eric@nemchik.com>
2022-05-07 15:59:05 -0500

    Ts more configs again (#4567)
    * tsconfig comments
    also recreate tsconfig base from tsc --init
    * Add eslint-define-config
    Rewrite eslintrc
    * More tsconfig adjustments
    * Move bad tsRules to separate var

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,101 +1,18 @@
-const projects = ["./tsconfig.json", "./client/tsconfig.json", "./src/tsconfig.json"];
+// @ts-check
+const {defineConfig} = require("eslint-define-config");
 
-module.exports = {
-	root: true,
-	overrides: [
-		{
-			files: [
-				"**/*.ts",
-				// "**/*.vue"
-			],
-			parser: "@typescript-eslint/parser",
-			parserOptions: {
-				tsconfigRootDir: __dirname,
-				project: ["./tsconfig.json", "./client/tsconfig.json", "./src/tsconfig.json"],
-				extraFileExtensions: [".vue"],
-				projects: projects,
-			},
-			plugins: ["@typescript-eslint"],
-			extends: [
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
-				"prettier",
-			],
-			rules: {
-				// note you must disable the base rule as it can report incorrect errors
-				"no-shadow": "off",
-				"@typescript-eslint/no-shadow": ["error"],
-				// TODO: eventually remove these
-				"@typescript-eslint/ban-ts-comment": "off",
-				"@typescript-eslint/no-explicit-any": "off",
-				"@typescript-eslint/no-non-null-assertion": "off",
-				"@typescript-eslint/no-this-alias": "off",
-				"@typescript-eslint/no-unnecessary-type-assertion": "off",
-				"@typescript-eslint/no-unsafe-argument": "off",
-				"@typescript-eslint/no-unsafe-assignment": "off",
-				"@typescript-eslint/no-unsafe-call": "off",
-				"@typescript-eslint/no-unsafe-member-access": "off",
-				"@typescript-eslint/no-unused-vars": "off",
-			},
-		},
-		// TODO: verify
-		{
-			files: ["**/*.vue"],
-			parser: "vue-eslint-parser",
-			parserOptions: {
-				ecmaVersion: 2022,
-				ecmaFeatures: {
-					jsx: true,
-				},
-				parser: {
-					// Script parser for `<script>`
-					js: "espree",
-
-					// Script parser for `<script lang="ts">`
-					ts: "@typescript-eslint/parser",
-
-					// Script parser for vue directives (e.g. `v-if=` or `:attribute=`)
-					// and vue interpolations (e.g. `{{variable}}`).
-					// If not specified, the parser determined by `<script lang ="...">` is used.
-					"<template>": "espree",
-				},
-				tsconfigRootDir: __dirname,
-				project: projects,
-			},
-			plugins: ["vue"],
-			extends: [
-				"plugin:vue/recommended",
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
-				"prettier",
-			],
-			rules: {
-				"import/no-default-export": 0,
-				"import/unambiguous": 0, // vue SFC can miss script tags
-				"@typescript-eslint/prefer-readonly": 0, // can be used in template
-				"vue/component-tags-order": [
-					"error",
-					{
-						order: ["template", "style", "script"],
-					},
-				],
-				"vue/multi-word-component-names": "off",
-				"vue/no-mutating-props": "off",
-				"vue/no-v-html": "off",
-				"vue/require-default-prop": "off",
-				"vue/v-slot-style": ["error", "longform"],
-			},
-		},
-	],
-	env: {
-		es6: true,
-		browser: true,
-		mocha: true,
-		node: true,
+const projects = defineConfig({
+	parserOptions: {
+		project: [
+			"./tsconfig.json",
+			"./client/tsconfig.json",
+			"./src/tsconfig.json",
+			"./test/tsconfig.json",
+		],
 	},
-	extends: ["eslint:recommended", "prettier"],
+}).parserOptions.project;
+
+const baseRules = defineConfig({
 	rules: {
 		"block-scoped-var": "error",
 		curly: ["error", "all"],
@@ -149,4 +66,104 @@ module.exports = {
 		strict: "off",
 		yoda: "error",
 	},
-};
+}).rules;
+
+const vueRules = defineConfig({
+	rules: {
+		"import/no-default-export": 0,
+		"import/unambiguous": 0, // vue SFC can miss script tags
+		"@typescript-eslint/prefer-readonly": 0, // can be used in template
+		"vue/component-tags-order": [
+			"error",
+			{
+				order: ["template", "style", "script"],
+			},
+		],
+		"vue/multi-word-component-names": "off",
+		"vue/no-mutating-props": "off",
+		"vue/no-v-html": "off",
+		"vue/require-default-prop": "off",
+		"vue/v-slot-style": ["error", "longform"],
+	},
+}).rules;
+
+const tsRules = defineConfig({
+	rules: {
+		// note you must disable the base rule as it can report incorrect errors
+		"no-shadow": "off",
+		"@typescript-eslint/no-shadow": ["error"],
+	},
+}).rules;
+
+const tsRulesTemp = defineConfig({
+	rules: {
+		// TODO: eventually remove these
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-this-alias": "off",
+		"@typescript-eslint/no-unnecessary-type-assertion": "off",
+		"@typescript-eslint/no-unsafe-argument": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
+		"@typescript-eslint/no-unsafe-call": "off",
+		"@typescript-eslint/no-unsafe-member-access": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+	},
+}).rules;
+
+module.exports = defineConfig({
+	root: true,
+	overrides: [
+		{
+			files: [
+				"**/*.ts",
+				// "**/*.vue"
+			],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: projects,
+				extraFileExtensions: [".vue"],
+			},
+			plugins: ["@typescript-eslint"],
+			extends: [
+				"eslint:recommended",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"prettier",
+			],
+			rules: {...baseRules, ...tsRules, ...tsRulesTemp},
+		},
+		// TODO: verify
+		{
+			files: ["**/*.vue"],
+			parser: "vue-eslint-parser",
+			parserOptions: {
+				ecmaVersion: 2022,
+				ecmaFeatures: {
+					jsx: true,
+				},
+				parser: "@typescript-eslint/parser",
+				tsconfigRootDir: __dirname,
+				project: projects,
+			},
+			plugins: ["vue"],
+			extends: [
+				"eslint:recommended",
+				"plugin:vue/recommended",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"prettier",
+			],
+			rules: {...baseRules, ...tsRules, ...tsRulesTemp, ...vueRules},
+		},
+	],
+	env: {
+		es6: true,
+		browser: true,
+		mocha: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "prettier"],
+	rules: baseRules,
+});
