19d5beb562790fda845fb1a2e5f5e920f0a3db8b
Max Leiter <maxwell.leiter@gmail.com>
2022-06-01 11:45:08 -0700

    Replace vetur with volar, small types cleanup

--- client/js/types.d.ts
+++ client/js/types.d.ts
@@ -8,13 +8,6 @@ import {Mention} from "../../src/client";
 import {ClientConfiguration} from "../../src/server";
 import {LinkPreview} from "../../src/plugins/irc-events/link";
 
-// declare module '*.vue' {
-// 	import { defineComponent } from 'vue';
-
-// 	const component: ReturnType<typeof defineComponent>;
-// 	export default component;
-// }
-
 interface LoungeWindow extends Window {
 	g_TheLoungeRemoveLoading?: () => void;
 	navigator: Window["navigator"] & {
@@ -73,18 +66,6 @@ type ClientMention = Mention & {
 type ClientLinkPreview = LinkPreview & {
 	sourceLoaded?: boolean;
 };
-
-declare module "*.vue" {
-	const Component: ReturnType<typeof defineComponent>;
-	export default Component;
-}
-
-declare module "vue" {
-	// interface ComponentCustomProperties {
-	// 	// TODO: Vue struggles with typing using the options API, so we should switch to composition API
-	// 	// $root
-	// }
-}
 
 declare module "vue-router" {
 	import Vue from "./vue";
--- vetur.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	projects: ["./client/tsconfig.json"],
-};
