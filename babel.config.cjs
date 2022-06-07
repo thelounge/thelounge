1fc7b64ea838dc380918436069007cd3aea903dc
Max Leiter <maxwell.leiter@gmail.com>
2022-06-07 12:25:02 -0700

    cleanup babel deps

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,4 @@
 module.exports = {
-	presets: [
-		["@babel/preset-env", {bugfixes: true}],
-		"babel-preset-typescript-vue3",
-		"@babel/preset-typescript", // ? babel-preset-typescript-vue should be a drop-in replacement for @babel/typescript with vue support
-		// "@vue/babel-preset-jsx",
-	],
+	presets: [["@babel/preset-env", {bugfixes: true}], "babel-preset-typescript-vue3"],
 	plugins: ["@babel/plugin-transform-runtime"],
 };
