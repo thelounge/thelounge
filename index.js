eadd4905272709b00f76cc102f9186e45f159d7e
Max Leiter <maxwell.leiter@gmail.com>
2022-06-08 00:23:25 -0700

    move version check from src/index to index.js

--- index.js
+++ index.js
@@ -4,6 +4,27 @@
 
 process.chdir(__dirname);
 
+// Perform node version check before loading any other files or modules
+// Doing this check as soon as possible allows us to
+// avoid ES6 parser errors or other issues
+const pkg = require("./package.json");
+
+if (!require("semver").satisfies(process.version, pkg.engines.node)) {
+	/* eslint-disable no-console */
+	console.error(
+		"The Lounge requires Node.js " +
+			pkg.engines.node +
+			" (current version: " +
+			process.version +
+			")"
+	);
+	console.error("Please upgrade Node.js in order to use The Lounge");
+	console.error("See https://thelounge.chat/docs/install-and-upgrade");
+	console.error();
+
+	process.exit(1);
+}
+
 const fs = require("fs");
 
 if (fs.existsSync("./src/dist/src/index.js")) {
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
 #!/usr/bin/env ts-node
 
-// Perform node version check before loading any other files or modules
-// Doing this check as soon as possible allows us to
-// avoid ES6 parser errors or other issues
-import pkg from "../package.json";
-import {satisfies} from "semver";
-
-if (!satisfies(process.version, pkg.engines.node)) {
-	/* eslint-disable no-console */
-	console.error(
-		"The Lounge requires Node.js " +
-			pkg.engines.node +
-			" (current version: " +
-			process.version +
-			")"
-	);
-	console.error("Please upgrade Node.js in order to use The Lounge");
-	console.error("See https://thelounge.chat/docs/install-and-upgrade");
-	console.error();
-
-	process.exit(1);
-}
-
 import * as dns from "dns";
 
 // Set DNS result order early before anything that may depend on it happens.
