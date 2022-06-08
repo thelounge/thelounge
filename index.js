b286e29707f763a5198b995f3c871e93dc8fc6a3
Max Leiter <maxwell.leiter@gmail.com>
2022-06-08 00:27:58 -0700

    remove unnecessary eslint-disable in index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const fs = require("fs");
 if (fs.existsSync("./src/dist/src/index.js")) {
 	require("./src/dist/src/index.js");
 } else {
-	// eslint-disable-next-line no-console
 	console.error(
 		"Files in ./src/dist/src/ not found. Please run `yarn build` before trying to run `node index.js`."
 	);
