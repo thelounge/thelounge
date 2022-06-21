a15ac88ff2a7217156e7781df5cf540ad316b77f
Pavel Djundik <xPaw@users.noreply.github.com>
2022-06-21 10:51:24 +0300

    Fix user commands not working
    Fixes #4593

--- server/command-line/users/edit.ts
+++ server/command-line/users/edit.ts
@@ -18,7 +18,7 @@ program
 		}
 
 		// eslint-disable-next-line @typescript-eslint/no-var-requires
-		const ClientManager = require("../../clientManager");
+		const ClientManager = require("../../clientManager").default;
 		const users = new ClientManager().getUsers();
 
 		if (users === undefined) {
