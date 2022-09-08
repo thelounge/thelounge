6221f96ff29acf690ca9045c21a8fc9622cfc589
Max Leiter <maxwell.leiter@gmail.com>
2022-09-07 21:26:29 -0700

    Disable connecting to the same network multiple times if lockNetwork is enabled

--- server/server.ts
+++ server/server.ts
@@ -478,6 +478,12 @@ function initializeClient(
 	});
 
 	socket.on("network:new", (data) => {
+		if (Config.values.lockNetwork) {
+			if (client.networks.length > 0) {
+				return;
+			}
+		}
+
 		if (_.isPlainObject(data)) {
 			// prevent people from overriding webirc settings
 			data.uuid = null;
