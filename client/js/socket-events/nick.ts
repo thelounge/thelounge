d58aa3494ff425f1a485be59c4c5ebca650c3fc8
Callum Williamon <ctw.odiham@gmail.com>
2022-10-15 00:06:29 +0100

    POC API Integration

--- client/js/socket-events/nick.ts
+++ client/js/socket-events/nick.ts
@@ -1,8 +1,16 @@
 import socket from "../socket";
 import {store} from "../store";
+import httpReq from "../helpers/httpReq";
 
 socket.on("nick", function (data) {
 	const network = store.getters.findNetwork(data.network);
+	// eslint-disable-next-line no-console
+	console.log(data.nick);
+
+	httpReq(data.nick, "GET", "http://localhost:8080/api/v1/ping", {}, {}, (rsp, obj) => {
+		// eslint-disable-next-line no-console
+		console.log(obj.msg);
+	});
 
 	if (network) {
 		network.nick = data.nick;
