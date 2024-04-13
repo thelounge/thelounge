0311e5f836846a534bf5d8a23a94437133cda1c0
Reto Brunner <reto@slightlybroken.com>
2024-04-13 22:42:24 +0200

    add socket-events import to entry point
    socket-events aren't ever imported, if we don't do that however
    webpack never actually sees any code that leads to it and skips
    bundling it.
    
    So for now, do an import that has the side effect of registering
    all the events until we have a proper registration in place that's
    a bit more sane to call

--- client/js/vue.ts
+++ client/js/vue.ts
@@ -7,6 +7,7 @@ import App from "../components/App.vue";
 import storage from "./localStorage";
 import {router} from "./router";
 import socket from "./socket";
+import "./socket-events"; // this sets up all socket event listeners, do not remove
 import eventbus from "./eventbus";
 
 import "./webpush";
