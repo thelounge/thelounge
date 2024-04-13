9f2c82e152fed055247667286649329dfeb0409c
Reto Brunner <reto@slightlybroken.com>
2024-04-14 00:52:16 +0200

    fix mentions

--- client/js/socket-events/mentions.ts
+++ client/js/socket-events/mentions.ts
@@ -1,7 +1,17 @@
 import socket from "../socket";
 import {store} from "../store";
 import {ClientMention} from "../types";
+import {SharedMention} from "../../../shared/types/mention";
 
 socket.on("mentions:list", function (data) {
-	store.commit("mentions", data as ClientMention[]);
+	store.commit("mentions", data.map(sharedToClientMention));
 });
+
+function sharedToClientMention(shared: SharedMention): ClientMention {
+	const mention: ClientMention = {
+		...shared,
+		localetime: "", // TODO: can't be right
+		channel: null,
+	};
+	return mention;
+}
