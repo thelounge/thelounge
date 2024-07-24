a61bc14456dcaf335884e62cabb1368df5db9b41
Reto Brunner <reto@slightlybroken.com>
2024-07-24 19:42:01 +0200

    remove dns resolution order override
    This is the default for node 18.

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,1 @@
-import * as dns from "dns";
-
-// Set DNS result order early before anything that may depend on it happens.
-if (dns.setDefaultResultOrder) {
-	dns.setDefaultResultOrder("verbatim");
-}
-
 import "./command-line";
