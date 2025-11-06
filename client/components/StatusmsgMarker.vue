0d7befc7b45e147aa4818d05273a7efd06666131
Taavi Väänänen <hi@taavi.wtf>
2025-11-06 02:31:25 +0200

    Show STATUSMSG indication for actions as well
    It is possible to send actions (/me et al.) to a STATUSMSG target
    (+#channel etc), even though thelounge currently does not support
    sending those. Show the statusmsg indicator on such messages.

---
+++ client/components/StatusmsgMarker.vue
@@ -0,0 +1,19 @@
+<template>
+	<span
+		v-if="group"
+		:aria-label="`This message was only shown to users with ${group} mode`"
+		class="msg-statusmsg tooltipped tooltipped-e"
+		><span>{{ group }}</span></span
+	>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+	name: "StatusmsgMarker",
+	props: {
+		group: {type: String, required: true},
+	},
+});
+</script>
