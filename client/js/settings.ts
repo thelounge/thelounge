f73aa15e3cc34c2f2a8b1b0f74f9f457e0ff443f
kofany <j@dabrowski.biz>
2025-11-20 18:01:09 +0100

    Add type annotations for improved type safety
    Introduced explicit type definitions in Account.vue, router.ts, settings.ts, and connection.ts to improve type safety and code clarity. Replaced 'any' with more specific types and interfaces where applicable.

--- client/js/settings.ts
+++ client/js/settings.ts
@@ -159,7 +159,7 @@ export function createState() {
 	return state;
 }
 
-function normalizeConfig(obj: any) {
+function normalizeConfig(obj: Record<string, Record<string, unknown>>) {
 	const newConfig: Partial<typeof defaultConfig> = {};
 
 	for (const settingName in obj) {
