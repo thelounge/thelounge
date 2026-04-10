ca5f7bb37d893a55d34a075b1cd7c170c5433664
Max Leiter <8675906+MaxLeiter@users.noreply.github.com>
2026-04-10 00:35:24 -0700

    theme

--- server/plugins/irc-events/standard-reply.ts
+++ server/plugins/irc-events/standard-reply.ts
@@ -3,15 +3,16 @@ import {IrcEventHandler} from "../../client";
 import Msg from "../../models/msg";
 import {MessageType} from "../../../shared/types/msg";
 
+const typeMap: Record<string, MessageType> = {
+	FAIL: MessageType.ERROR,
+	WARN: MessageType.WARN,
+	NOTE: MessageType.NOTE,
+};
+
 export default <IrcEventHandler>function (irc, network) {
 	const client = this;
 
 	irc.on("standard reply", function (data) {
-		const typeMap: Record<string, MessageType> = {
-			FAIL: MessageType.ERROR,
-			WARN: MessageType.WARN,
-			NOTE: MessageType.NOTE,
-		};
 		const type = typeMap[data.type] || MessageType.ERROR;
 
 		let target = network.getLobby();
