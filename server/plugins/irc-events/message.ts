26ed4cf93e2170d8b524002db4bf5535bb7ac32f
Max Leiter <8675906+MaxLeiter@users.noreply.github.com>
2026-04-09 22:29:36 -0700

    undo

--- server/plugins/irc-events/message.ts
+++ server/plugins/irc-events/message.ts
@@ -126,12 +126,8 @@ export default <IrcEventHandler>function (irc, network) {
 		}
 
 		// https://ircv3.net/specs/extensions/bot-mode
-		if (data.tags && "bot" in data.tags && !from.isBot) {
+		if (data.tags && "bot" in data.tags) {
 			from.isBot = true;
-
-			client.emit("users", {
-				chan: chan.id,
-			});
 		}
 
 		// msg is constructed down here because `from` is being copied in the constructor
