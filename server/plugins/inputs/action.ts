5d8c6c671ad97eda2d43c64adb73a809658fbe9c
Max Leiter <8675906+MaxLeiter@users.noreply.github.com>
2026-05-02 14:15:41 -0700

    deps: bump irc-framework pin to include tags-action; use action() with tags
    irc-framework PR #411 lets action() accept tags directly, replacing
    the sendMessage('PRIVMSG', ..., '\x01ACTION ...\x01', tags) workaround
    that was needed to attach +reply tags to /me messages.

--- server/plugins/inputs/action.ts
+++ server/plugins/inputs/action.ts
@@ -37,13 +37,7 @@ const input: PluginInputHandler = function (network, chan, cmd, args, extras) {
 			const replyTags =
 				replyTo && network.serverOptions.supportsReply ? {"+reply": replyTo} : undefined;
 
-			if (replyTags) {
-				// irc.action() doesn't support tags
-				// TODO: use action() once this merges: https://github.com/kiwiirc/irc-framework/pull/411
-				irc.sendMessage("PRIVMSG", chan.name, "\x01ACTION " + text + "\x01", replyTags);
-			} else {
-				irc.action(chan.name, text);
-			}
+			irc.action(chan.name, text, replyTags);
 
 			// If the IRCd does not support echo-message, simulate the message
 			// being sent back to us.
